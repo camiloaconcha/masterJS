b928b5a5a895363ace4be53cee3979e770732aac
Camilo Andrés Concha Agudelo <camiloandres.concha@bbva.com>
2018-09-17 14:49:21 -0500

    Ejercicio 2

--- js/ejercicios/ejercicio2.js
+++ js/ejercicios/ejercicio2.js
@@ -2,3 +2,24 @@
 /*Utilizando un bucle ,mostrar la suma y 
 la media de los numeros introducido hasta encontrar uno negativo y
 mostrar el resultado*/
+
+var suma = 0;
+var contador = 0;
+
+do {
+    var numero = parseInt(prompt('Introduce números hasta encontrar uno negativo',0));
+
+    if (isNan(numero)) {
+        numero = 0;
+    } else if(numero >= 0){
+        suma += numero;    
+
+        contador++;
+    }
+    console.log(suma);
+    console.log(numero);
+} while (numero >= 0);
+
+
+alert("Suma:"+suma);
+alert("Media"+(suma/contador));
